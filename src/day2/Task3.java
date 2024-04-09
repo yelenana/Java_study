a5b411a0830912e6158993e15bdd478b05f3d3f6
Olena Rybchynska <rybchinskayalena@gmail.com>
2024-04-09 23:38:16 +0300

    task3 added

--- src/day2/Task3.java
+++ src/day2/Task3.java
@@ -2,20 +2,21 @@ package day2;
 
 import java.util.Scanner;
 
-public class Task2 {
+public class Task3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int inputFirst = scanner.nextInt();
         int inputSecond = scanner.nextInt();
 
-        for (int i = inputFirst + 1; i < inputSecond; i++) {
-            if (i % 10 == 0) {
-                continue;
-            } else if (i % 5 == 0) {
-                System.out.print(i + " ");
+        int i = inputFirst+1;
+
+        while (i<inputSecond) {
+            
+            if (i % 10 != 0 && i % 5 == 0) {
                 System.out.print(i + " ");
             }
+            i++;
         }
-
     }
+
 }
